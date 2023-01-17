c8d122afe4fbc5885380395f9bd67a0875a00547
LuisDani07 <tics270118@gmail.com>
2023-01-16 19:21:23 -0700

    estilos del header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Header from './components/Header';
+import Watchlist from './components/Watchlist';
+import Watched from './components/Watched';
+import Add from './components/Add';
+import './App.css'
 function App() {
 
 
   return (
      <>
-         <h1>hello world</h1>
+         <Router>
+                 <Header/>
+                 <Routes>
+                        <Route exact path='/' element={<Watchlist/>}/>
+                        <Route  path='/watched' element={<Watched/>}/>
+                        <Route  path='/add' element={<Add/>}/>
+                 </Routes>
+         </Router>
      </>
   )
 }
