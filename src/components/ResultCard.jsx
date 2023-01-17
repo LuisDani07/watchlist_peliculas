f6e5c03e69bc1d141f64265d035e50567aaddb18
LuisDani07 <tics270118@gmail.com>
2023-01-17 15:38:30 -0700

    botón para añadir peliculas

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -13,8 +13,15 @@ function ResultCard({movie}) {
         <div className="info">
             <div className="header">
                 <h3 className="title">{movie.title}</h3>
-                <h4 className='release-date'>{movie.release_date}</h4>
+                <h4 className='release-date'>
+                 {movie.release_date?  movie.release_date.substring(0,4): "-"}
+                </h4>
             </div>
+              <div className="controls">
+                   <button className="btn">
+                    Add to watchlist
+                   </button>
+              </div>
         </div>
     </div>
   )
---
+++ src/context/Global_State.jsx
@@ -0,0 +1,9 @@
+import React,{createContext,useReducer,useEffect} from 'react'
+
+function global_state() {
+  return (
+    <div>global_state</div>
+  )
+}
+
+export default global_state
