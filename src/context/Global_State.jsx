babb02cdb4b75e0de9462bb491b4b8bfe86c3272
LuisDani07 <tics270118@gmail.com>
2023-01-21 22:37:09 -0700

    guardandoe el estado en localstorage

--- src/context/Global_State.jsx
+++ src/context/Global_State.jsx
@@ -4,8 +4,8 @@ import AppReducer from './AppReducer'
 
 //initial state
 const initialState={
-    watchlist:[],
-    watched:[],
+    watchlist:localStorage.getItem('watchlist') ? JSON.parse(localStorage.getItem('watchlist')): [],
+    watched:localStorage.getItem('watched') ? JSON.parse(localStorage.getItem('watched')): []
 };
 
 //create context
@@ -18,6 +18,11 @@ export const globalContext=createContext(initialState);
 
 export const GlobalProvider=(props)=>{
     const [state, dispatch]=useReducer(AppReducer, initialState);
+
+    useEffect(()=>{
+        localStorage.setItem('watchlist', JSON.stringify(state.watchlist))
+        localStorage.setItem('watched', JSON.stringify(state.watched))
+    },[state])
 
     //actions
 
