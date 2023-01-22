7e155c70ee954b61d642af57e89f6a83bbc24af2
LuisDani07 <tics270118@gmail.com>
2023-01-22 11:30:06 -0700

    pasando contex api a watchlist

--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import {globalContext} from '../context/Global_State'
 
 function Watchlist() {
+  const {watchlist}=useContext(globalContext)
   return (
-    <div>Watchlist</div>
+       <div className="movie-page">
+        <div className="container">
+          <div className="header">
+            <h1 className="heading">My Watchlist</h1>
+          </div>
+          
+        </div>
+       </div>
   )
 }
 
