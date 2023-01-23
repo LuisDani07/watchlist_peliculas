16527063c35ecb2baf64c8f188b9d600249cdbc5
LuisDani07 <tics270118@gmail.com>
2023-01-23 12:01:53 -0700

    movie card component con sus estilos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function MovieCard({movie, type}) {
   return (
-    <div movie-card>
-        <div className="overlay"></div>
-        {movie.poster_path ? (
+      <div className="movie-card">
+          <div className='overlay'></div>
+          {movie.poster_path ? (
                <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={`${movie.title} Poster`} /> 
             ):(
                 <div className="filler-poster"></div>
             )}
-    </div>
+      </div>
   )
 }
 
--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -1,22 +1,28 @@
 import React, {useContext} from 'react';
 import {globalContext} from '../context/Global_State'
-import MovieCard from './MovieCard'
+import MovieCard from './MovieCard';
 
 
 function Watchlist() {
   const {watchlist}=useContext(globalContext)
   return (
        <div className="movie-page">
-        <div className="container">
-          <div className="header">
-            <h1 className="heading">My Watchlist</h1>
-          </div>
-            <div className="movie-grid">
-              {watchlist.map((movie)=>{
-                <MovieCard movie={movie}/>
-              })}
+          <div className="container">
+            <div className="header">
+               <h1 className='heading'>My watchlist</h1>
             </div>
-        </div>
+             {watchlist.length>0 ?(
+                      <div className='movie-grid'>
+                      {watchlist.map((movie)=>(
+                         <MovieCard movie={movie} type="watchlist"/>
+                      ))}
+                    </div>
+             ):
+             (
+              <h2 className='no-movies'>no movies in your list, add some</h2>
+             )}
+          
+          </div>
        </div>
   )
 }
